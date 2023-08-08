69adfbfd21b7692c2035978973b245a8e1b29600
Dominowa <babicyura@gmail.com>
2023-08-08 15:46:50 +0300

    home work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+function getCurrentTime() {
+  return player.getCurrentTime();
+}
+
+function saveCurrentTime(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+}
+
+function restorePlaybackPosition() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime !== null) {
+    player.setCurrentTime(savedTime);
+  }
+}
+
+const player = new Vimeo(document.getElementById('vimeo-player'));
+
+const throttledSaveCurrentTime = throttle(saveCurrentTime, 1000);
+
+player.on('timeupdate', () => {
+  const currentTime = getCurrentTime();
+  throttledSaveCurrentTime(currentTime);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  restorePlaybackPosition();
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
+function loadFormState() {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const { email, message } = JSON.parse(savedState);
+    emailInput.value = email || '';
+    messageInput.value = message || '';
+  }
+}
+
+function saveFormState() {
+  const currentState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+}
+
+function clearFormState() {
+  localStorage.removeItem('feedback-form-state');
+}
+
+const throttledSaveFormState = throttle(saveFormState, 500);
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadFormState();
+});
+
+form.addEventListener('input', () => {
+  throttledSaveFormState();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  console.log('Form submitted with values:');
+  console.log('Email:', emailInput.value);
+  console.log('Message:', messageInput.value);
+
+  form.reset();
+  clearFormState();
+});
