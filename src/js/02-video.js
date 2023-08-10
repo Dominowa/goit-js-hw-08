90274d876408e8c6094b4d61742d6a322f3c4e48
Dominowa <babicyura@gmail.com>
2023-08-10 15:01:01 +0300

    up

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,25 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const player = new Vimeo(document.getElementById('vimeo-player'));
+const THROTTLE_TIME = 1000;
+
 function getCurrentTime() {
   return player.getCurrentTime();
 }
 
 function saveCurrentTime(time) {
-  localStorage.setItem('videoplayer-current-time', time);
+  sessionStorage.setItem('videoplayer-current-time', time);
 }
 
 function restorePlaybackPosition() {
-  const savedTime = localStorage.getItem('videoplayer-current-time');
+  const savedTime = sessionStorage.getItem('videoplayer-current-time');
   if (savedTime !== null) {
     player.setCurrentTime(savedTime);
   }
 }
 
-const player = new Vimeo(document.getElementById('vimeo-player'));
-
-const throttledSaveCurrentTime = throttle(saveCurrentTime, 1000);
+const throttledSaveCurrentTime = throttle(saveCurrentTime, THROTTLE_TIME);
 
 player.on('timeupdate', () => {
   const currentTime = getCurrentTime();
